40e835a71a742427fcde2358dd5e5a34af8b3a28
Sabyrbek.Ukubaev <sakuba61@gmail.com>
2025-05-14 11:59:30 +0500

    autoload

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,13 @@
 <?php
 
-declare(strict_types = 1);
 
-phpinfo();
+spl_autoload_register(function($class){
+    $path = __DIR__ . '/../' . lcfirst(str_replace('\\', '/', $class) . '.php');
+    require $path;
+    var_dump($path);
+    echo '<br />Autoload <br />';
+});
+
+use App\PaymentGateway\Puddle\Transaction;
+
+var_dump(new Transaction());
